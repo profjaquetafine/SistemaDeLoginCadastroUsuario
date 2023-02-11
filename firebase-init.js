de8f8b7defb56d07d0f2fab77d3a88047dc9461d
profjaquetafine <88409934+profjaquetafine@users.noreply.github.com>
2023-02-10 21:22:56 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTWPnEckgeAsnxGOvBcnZ3rS-CceNUUaY",
+    authDomain: "sistemafinanceiro-4bfcc.firebaseapp.com",
+    projectId: "sistemafinanceiro-4bfcc",
+    storageBucket: "sistemafinanceiro-4bfcc.appspot.com",
+    messagingSenderId: "1092408663475",
+    appId: "1:1092408663475:web:652fe8d1872c5d299bffc6"
+  };
+firebase.initializeApp(firebaseConfig);
